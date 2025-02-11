6f586658e0eb342a93caf64b676d4a306ec4738e
Franchesco <steven777p@gmail.com>
2025-02-11 16:33:37 -0500

    :crocodile: Docker-compose of BusManagement is ready

--- BusManagement/ViewBusDetails/src/config/db.js
+++ BusManagement/ViewBusDetails/src/config/db.js
@@ -4,14 +4,15 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connection = mysql.createPool({
-    host: process.env.DB_HOST || 'busmanagement.ckkft1kjllti.us-east-1.rds.amazonaws.com', // Aquí usas el endpoint de RDS
-    user: process.env.DB_USER || 'admin', // Usa tu usuario de RDS
-    password: process.env.DB_PASSWORD || 'tu-contraseña', // La contraseña de RDS
-    database: process.env.DB_NAME || 'bus_management', // Nombre de tu base de datos
-    port: process.env.DB_PORT || 3306, // Puerto 3306 por defecto para MySQL
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
+
 
 module.exports = connection;
