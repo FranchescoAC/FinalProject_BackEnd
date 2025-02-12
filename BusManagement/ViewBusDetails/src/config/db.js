96d3a459411129af9a5482c7a01e9b4d8ae993cf
Felipe0806 <felipe.almeida9b@gmail.com>
2025-02-11 21:55:10 -0500

    fixed

--- BusManagement/ViewBusDetails/src/config/db.js
+++ BusManagement/ViewBusDetails/src/config/db.js
@@ -1,14 +1,18 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2/promise'); // Usa mysql2/promise
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const connection = mysql.createPool({
-    host: 'busmanagement.ckkft1kjllti.us-east-1.rds.amazonaws.com', // RDS endpoint
-    user: 'root', // RDS user
-    password: 'root12345678', // RDS password
-    database: 'bus_management', // Database name
-    port: 3306, // Default port for MySQL
-    waitForConnections: true, // Allow requests to wait if no connections are available
-    connectionLimit: 10, // Maximum number of simultaneous connections
-    queueLimit: 0 // No limit on the number of queued requests
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
+
 
 module.exports = connection;
